ef93872275bfc547f8a4f33460807283ce23d6eb
ishika <ishikajaveri96@gmail.com>
2023-11-26 10:16:11 +0530

    add a static html page to display songs

--- resources/views/songs_static.blade.php
+++ resources/views/songs_static.blade.php
@@ -1,121 +1,142 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <style>
-    body {
-      margin: 0;
-      font-family: 'Arial', sans-serif;
-    }
-
-    .wrapper {
-      display: grid;
-      grid-template-columns: 1fr 3fr; /* Adjust column widths as needed */
-      grid-template-rows: auto 1fr auto;
-      min-height: 100vh;
-    }
-
-    header, aside, main, footer {
-      padding: 20px;
-    }
-
-    header, footer {
-      background-color: #333;
-      color: #fff;
-    }
-
-    header h1 {
-      margin: 0;
-    }
-
-    aside, main {
-      border-right: 1px solid #ccc;
-    }
-
-    table {
-      width: 100%;
-      border-collapse: collapse;
-      margin-top: 20px;
-    }
-
-    table, th, td {
-      border: 1px solid #ccc;
-    }
-
-    th, td {
-      padding: 10px;
-      text-align: left;
-    }
-
-    footer {
-      background-color: #333;
-      color: #fff;
-    }
-
-    nav ul {
-      list-style: none;
-      padding: 0;
-      display: flex;
-      justify-content: space-around;
-    }
-
-    nav a {
-      color: #fff;
-      text-decoration: none;
-    }
-
-    nav a:hover {
-      text-decoration: underline;
-    }
-  </style>
-  <title>Your Playlist App</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Playlist Viewer</title>
+    <style>
+        body {
+            font-family: 'Arial', sans-serif;
+            margin: 0;
+            padding: 0;
+            display: grid;
+            grid-template-columns: 250px 1fr;
+            grid-template-rows: auto 1fr auto;
+            min-height: 100vh;
+            background: radial-gradient(circle at 24.1% 68.8%, rgb(50, 50, 50) 0%, rgb(0, 0, 0) 99.4%);
+        }
+        header{
+            grid-column: span 2;
+            text-align: left;
+            padding: 20px;
+            background-color: #000000;
+            background-image: linear-gradient(315deg, #000000 0%, #414141 74%);
+            color: white;
+            grid-row: 1;
+        }
+        footer {
+            grid-column: span 2;
+            text-align: center;
+            padding: 10px;
+            background-color: #333;
+            color: white;
+        }
+        main {
+            padding: 20px;
+            overflow: auto;
+        }
+        sidebar {
+            grid-row: 2;
+            background-color: #f4f4f4;
+            padding: 20px;
+            height: 100%;
+            display: flex;
+            flex-direction: column;
+        }
+        .nav-options {
+            margin-bottom: 20px;
+            border-bottom: 1px solid #ccc;
+            padding-bottom: 10px;
+        }
+        .playlist-item {
+            padding: 10px;
+            cursor: pointer;
+            transition: background-color 0.3s;
+        }
+        .playlist-item:hover {
+            background-color: #ddd;
+        }
+        table {
+            width: 100%;
+            border-collapse: collapse;
+            margin-top: 20px;
+            color: white;
+        }
+        th, td {
+            padding: 10px;
+            text-align: left;
+            border-bottom: 1px solid #ddd;
+        }
+        th {
+            background-color: #333;
+            color: white;
+        }
+        a{
+            text-decoration: none;
+            color: white;
+        }
+        @media (max-width: 768px) {
+            body {
+                grid-template-columns: 1fr;
+                grid-template-rows: auto auto 1fr auto;
+            }
+            sidebar {
+                grid-column: span 1;
+                height: auto;
+            }
+            header, footer {
+                grid-column: span 1;
+            }
+        }
+    </style>
 </head>
 <body>
-
-  <div class="wrapper">
     <header>
-      <h1>Playlist Title</h1>
-      <!-- Add other basic information here -->
+        <h5>Playlist</h6>
+        <h1>Daily Mix 1</h1>
+        <p>A playlist consisting of your fav artists</p>
     </header>
 
-    <aside>
-      <h2>Saved Playlists</h2>
-      <!-- Add your list of saved playlists here -->
-    </aside>
+    <sidebar>
+        <div class="nav-options">Home</div>
+        <div class="nav-options">Search</div>
+        <div class="nav-options">My Playlists</div>
+        <div class="playlist-item">Playlist 1</div>
+        <div class="playlist-item">Playlist 2</div>
+    </sidebar>
 
     <main>
-      <h2>Song List</h2>
-      <table>
-        <thead>
-          <tr>
-            <th>Serial No</th>
-            <th>Song Title</th>
-            <th>Artist Name</th>
-            <th>Album Name</th>
-            <th>Duration</th>
-            <!-- Add other table headers as needed -->
-          </tr>
-        </thead>
-        <tbody>
-          <!-- Add your list of songs here -->
-        </tbody>
-      </table>
+        <table>
+            <thead>
+                <tr>
+                    <th>#</th>
+                    <th>Title</th>
+                    <th>Artists</th>
+                    <th>Album</th>
+                    <th>Duration</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>1</td>
+                    <td>Song 1</td>
+                    <td>Artist 1</td>
+                    <td>Album 1</td>
+                    <td>3:30</td>
+                </tr>
+                <tr>
+                    <td>2</td>
+                    <td>Song 2</td>
+                    <td>Artist 2</td>
+                    <td>Album 2</td>
+                    <td>4:15</td>
+                </tr>
+            </tbody>
+        </table>
     </main>
 
     <footer>
-      <p>&copy; 2023 Your Playlist App</p>
-      <nav>
-        <ul>
-          <li><a href="#">About</a></li>
-          <li><a href="#">Terms</a></li>
-          <li><a href="#">Privacy</a></li>
-          <!-- Add other links as needed -->
-        </ul>
-      </nav>
+        <p>&copy; 2023 Playlist Viewer | <a href="#">About</a> | <a href="#">Privacy</a></p>
     </footer>
-  </div>
-
 </body>
 </html>
-
--- tests/Feature/SongsTest.php
+++ tests/Feature/SongsTest.php
@@ -1,25 +1,49 @@
-use PHPUnit\Framework\TestCase;
-use GuzzleHttp\Client;
-
+<?php
+namespace Tests\Feature;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+use Practicals\Song;
 class SongsTest extends TestCase
 {
-    protected $httpClient;
-
-    protected function setUp(): void
+    public function testSetTempo(): void
     {
-        // Initialize the HTTP client before each test
-        $this->httpClient = new Client([
-            'base_uri' => 'http://your-app-base-url', // Replace with the actual base URL of your application
-        ]);
+        $song = new Song("Title", "Artist", "Genre", 120);
+        $song->setTempo(60);
+        $result = $song->getTempo();
+        $this->assertEquals(60, $result);
+    }
+    public function testSetTempoAcceptsIntegerString(): void
+    {
+        $song = new Song("Title", "Artist", "Genre", 120);
+        $song->setTempo("60");
+        $result = $song->getTempo();
+        $this->assertEquals(60, $result);
+    }
+    public function testSetTempoRejectsAlphabetString(): void
+    {
+        $song = new Song("Title", "Artist", "Genre", 120);
+        $this->expectException(\InvalidArgumentException::class);
+        $song->setTempo("60 BPM");
+    }
+    public function testSetTempoRejectsFloat(): void
+    {
+        $song = new Song("Title", "Artist", "Genre", 120);
+        $this->expectException(\InvalidArgumentException::class);
+        $song->setTempo(60.5);
+    }
+    public function testSongsOk(): void
+    {
+        $response = $this->get('/songs');
+        $response->assertStatus(200);
     }
 
-    public function testSongsStaticEndpointReturns200()
+    public function testSongs_staticOk(): void
     {
-        // Send a request to the /songs_static endpoint
-        $response = $this->httpClient->get('/songs_static');
-
-        // Assert that the response status code is 404 (this is intentionally incorrect for a failing test)
-        $this->assertEquals(404, $response->getStatusCode());
+        $response = $this->get('/songs_static');
+    $response->assertStatus(200);
     }
+
+
 }
 
