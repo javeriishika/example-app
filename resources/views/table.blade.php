feb4d11b386e4b0b6f1f3307752b8e9d99a62765
ishika <ishikajaveri96@gmail.com>
2023-11-26 10:40:18 +0530

    routing assignments done

---
+++ resources/views/table.blade.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <style>
+        table{
+            width: 50%;
+            height: 50vh;
+            margin: 5em auto;
+            background-color: gray;
+            color: black;
+            font-weight: bolder;
+        }
+        td,tr,th{
+            text-align: center;
+            font-size: 1.2em;
+        }
+        th{
+            font-size: 1.7em;
+            background-color: darkgray;
+        }
+    </style>
+    <title>Document</title>
+</head>
+<body>
+    <table border=1>
+        <tr>
+            <th>Vegetable</th>
+            <th>Price</th>
+        </tr>
+        <tr>
+            <td>Baigan</td>
+            <td>100</td>
+        </tr>
+        <tr>
+            <td>Aaloo</td>
+            <td>1000</td>
+        </tr>
+        <tr>
+            <td>Bhindi</td>
+            <td>2</td>
+        </tr>
+        <tr>
+            <td>Gobhi</td>
+            <td>2</td>
+        </tr>
+    </table>
+</body>
+</html> 
+
--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,22 @@ use Practicals\Song;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/veggies/{sabji}', function ($sabji) {
+   
+    return $sabji;
+})->whereIn('sabji', ['baigan', 'bhindi', 'aaloo', 'gobhi']);
+Route::get('/greeting', function () {
+    return 'Hello World';
+});
+Route::get('/hello', function () {
+    return view('veggies');
+});
+
+// On route '/veggies 'a chart should be seen , with name and price of veggies
+Route::get('/veggies', function () {
+    return view('table');
+});
+
 Route::get('/songs', function () {
     return "Songs";
 });
